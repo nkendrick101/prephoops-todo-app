7ef93d90af609687906aa8665e81c05bed217a2d
Nico Kendrick <40567184+nkendrick101@users.noreply.github.com>
2021-02-26 11:29:01 -0600

    Create User.php

---
+++ hoopsapp/app/Http/Resources/User.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class User extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'email_verified_at' => $this->email_verified,
+            'password' => $this->password,
+            'user' => $this->user,
+        ];
+    }
+}
